360c2f78e168ae51d31c358360dee73ca8661d6b
Darin Scott Comeau <dcomeau@lanl.gov>
2018-02-23 15:17:15 -0700

    new experiment

--- all_regions_work.m
+++ all_regions_work.m
@@ -1,86 +1,86 @@
 % called by driver script to run analysis on all regions
 
-% region = 'Arctic'
+region = 'Arctic'
+calc_ica_pred
+calc_iva_pred
+close all
+
+% region = 'ChukchiBeaufort'
 % calc_ica_pred
 % calc_iva_pred
 % close all
 
-region = 'ChukchiBeaufort'
-calc_ica_pred
-calc_iva_pred
-close all
-
-region = 'Chukchi'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Chukchi'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
 region = 'Beaufort'
 calc_ica_pred
 calc_iva_pred
 close all
 
-region = 'EastSibLaptev'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'EastSibLaptev'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'EastSib'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'EastSib'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Laptev'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Laptev'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'BarentsKara'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'BarentsKara'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Barents'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Barents'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Kara'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Kara'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
 region = 'Greenland'
 calc_ica_pred
 calc_iva_pred
 close all
 
-region = 'Baffin'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Baffin'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Labrador'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Labrador'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Hudson'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Hudson'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Bering'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Bering'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'Okhotsk'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'Okhotsk'
+% calc_ica_pred
+% calc_iva_pred
+% close all
 
-region = 'CentralArctic'
-calc_ica_pred
-calc_iva_pred
-close all
+% region = 'CentralArctic'
+% calc_ica_pred
+% calc_iva_pred
+% close all
--- dowork.m
+++ dowork.m
@@ -9,13 +9,13 @@ fullDataOn = 0;
 trainLim = [100 499];
 testLim  = [500 899];
 
-% varsUsed = 'SIC'
-% embedWin = 12
-% all_regions_work
+varsUsed = 'SIC'
+embedWin = 12
+all_regions_work
 
-% varsUsed = 'SIC_SST'
-% embedWin = 12
-% all_regions_work
+varsUsed = 'SIC_SST'
+embedWin = 12
+all_regions_work
 
 % varsUsed = 'SIC_SST_SIT'
 % embedWin = 12
@@ -25,33 +25,33 @@ testLim  = [500 899];
 % embedWin = 12
 % all_regions_work
 
-% varsUsed = 'SIC_SIT'
-% embedWin = 12
-% all_regions_work
+varsUsed = 'SIC_SIT'
+embedWin = 12
+all_regions_work
 
-% varsUsed = 'SIC_SLP'
-% embedWin = 12
-% all_regions_work
+varsUsed = 'SIC_SLP'
+embedWin = 12
+all_regions_work
 
 % varsUsed = 'SIC_SST_SLP'
 % embedWin = 12
 % all_regions_work
 
 % % aggregate data
-% comp_data('SIC',12,'ica',fullDataOn)
-% comp_data('SIC',12,'iva',fullDataOn)
-% comp_data('SIC_SST',12,'ica',fullDataOn)
-% comp_data('SIC_SST',12,'iva',fullDataOn)
+comp_data('SIC',12,'ica',fullDataOn)
+comp_data('SIC',12,'iva',fullDataOn)
+comp_data('SIC_SST',12,'ica',fullDataOn)
+comp_data('SIC_SST',12,'iva',fullDataOn)
 % comp_data('SIC_SST_SIT',12,'ica',fullDataOn)
 % comp_data('SIC_SST_SIT',12,'iva',fullDataOn)
 % comp_data('SIC_SST_SIT_SLP',12,'ica',fullDataOn)
 % comp_data('SIC_SST_SIT_SLP',12,'iva',fullDataOn)
-% comp_data('SIC_SIT',12,'ica',fullDataOn)
-% comp_data('SIC_SIT',12,'iva',fullDataOn)
-% comp_data('SIC_SLP',12,'ica',fullDataOn)
-% comp_data('SIC_SLP',12,'iva',fullDataOn)
-comp_data('SIC_SST_SLP',12,'ica',fullDataOn)
-comp_data('SIC_SST_SLP',12,'iva',fullDataOn)
+comp_data('SIC_SIT',12,'ica',fullDataOn)
+comp_data('SIC_SIT',12,'iva',fullDataOn)
+comp_data('SIC_SLP',12,'ica',fullDataOn)
+comp_data('SIC_SLP',12,'iva',fullDataOn)
+% comp_data('SIC_SST_SLP',12,'ica',fullDataOn)
+% comp_data('SIC_SST_SLP',12,'iva',fullDataOn)
 
 % fullDataOn = 1;
 % comp_data('SIC',12,'ica',fullDataOn)
@@ -98,13 +98,13 @@ comp_data('SIC_SST_SLP',12,'iva',fullDataOn)
 % predOn = 1;
 % fullDataOn = 0;
 
-trainLim = [100 499];
-testLim  = [500 899];
-varsUsed = 'SIC_SST_SITq48'
-embedWin = 12
-all_regions_work
-comp_data('SIC_SST_SITq48',12,'ica',fullDataOn)
-comp_data('SIC_SST_SITq48',12,'iva',fullDataOn)
+% trainLim = [100 499];
+% testLim  = [500 899];
+% varsUsed = 'SIC_SST_SITq48'
+% embedWin = 12
+% all_regions_work
+% comp_data('SIC_SST_SITq48',12,'ica',fullDataOn)
+% comp_data('SIC_SST_SITq48',12,'iva',fullDataOn)
 
 
 
