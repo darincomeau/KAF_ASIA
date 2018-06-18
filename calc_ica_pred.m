cb05ab76c12700a73eaa86aea361958b2f114bfc
Darin Scott Comeau <dcomeau@lanl.gov>
2018-06-18 14:55:35 -0600

    Bug index fix

--- calc_ica_pred.m
+++ calc_ica_pred.m
@@ -480,9 +480,9 @@ if exist(checkFile) == 0 | predOn == 1
                 % pred_trajM(j,i) = pred_traj(initM + (i-1) + (j-1)*12, i);
                 % pred_trajDPM(j,i) = pred_trajDP(initM + (i-1) + (j-1)*12, i);
                 % truthM    (j,i) = truth    (initM + (i-1) + (j-1)*12, i);
-                pred_trajM(j,i) = pred_traj(initM - (i-1) + (j-1)*12, i);
-                pred_trajDPM(j,i) = pred_trajDP(initM - (i-1) + (j-1)*12, i);
-                truthM    (j,i) = truth    (initM - (i-1) + (j-1)*12, i);                
+                pred_trajM(j,i) = pred_traj(initM - (i-1) + j*12, i);
+                pred_trajDPM(j,i) = pred_trajDP(initM - (i-1) + j*12, i);
+                truthM    (j,i) = truth    (initM - (i-1) + j*12, i);                
             end
         end
 
